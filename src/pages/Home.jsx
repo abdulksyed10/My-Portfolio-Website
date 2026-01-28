44d48cb9de4e3ed7d346b133de537970a5ebc170
Abdul Kalam Syed <abdulksyed10@gmail.com>
2026-01-27 21:36:06 -0800

    Added contact page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { ThemeToggle } from "../components/ThemeToggle";
 import { AboutSection } from "../components/AboutSection";
 import { StarBackground } from "@/components/StarBackground";
 import { Navbar } from "../components/Navbar";
+import { ContactSection } from "../components/ContactSection";
 
 export const Home = () => {
   return (
@@ -19,6 +20,7 @@ export const Home = () => {
       {/* Main Content */}
       <main>
         <AboutSection />
+        <ContactSection />
       </main>
 
       {/* Footer */}
